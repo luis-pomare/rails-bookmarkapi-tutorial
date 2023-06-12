97ad829d38ed1035d319c1ae8fdd0ba67c7debfc
Luis Pomare <luispomare92@gmail.com>
2023-06-12 12:53:04 -0500

    Add authentication details to requests/create_spec

--- spec/requests/create_spec.rb
+++ spec/requests/create_spec.rb
@@ -1,49 +1,28 @@
 require 'rails_helper'
 
 describe 'POST /bookmarks' do
-  # 'scenario' is similar to 'it', use which you see fit
-  
+  # create a user before the test scenarios are run
+  let!(:user) { User.create(username: 'soulchild', authentication_token: 'abcdef') }
+
+  # pass the user username and authentication to the header
   scenario 'valid bookmark attributes' do
-    # send a POST request to /bookmarks, with these parameters
-    # The controller will treat them as JSON 
     post '/bookmarks', params: {
       bookmark: {
         url: 'https://rubyyagi.com',
         title: 'RubyYagi blog'
       }
-    }
-
-    # response should have HTTP Status 201 Created
-    expect(response.status).to eq(201)
-
-    json = JSON.parse(response.body).deep_symbolize_keys
-    
-    # check the value of the returned response hash
-    expect(json[:url]).to eq('https://rubyyagi.com')
-    expect(json[:title]).to eq('RubyYagi blog')
-
-    # 1 new bookmark record is created
-    expect(Bookmark.count).to eq(1)
-
-    # Optionally, you can check the latest record data
-    expect(Bookmark.last.title).to eq('RubyYagi blog')
+    }, headers: { 'X-Username': user.username, 'X-Token': user.authentication_token }
+  # ...
   end
 
+  # pass the user username and authentication to the header
   scenario 'invalid bookmark attributes' do
     post '/bookmarks', params: {
       bookmark: {
         url: '',
         title: 'RubyYagi blog'
       }
-    }
-
-    # response should have HTTP Status 201 Created
-    expect(response.status).to eq(422)
-
-    json = JSON.parse(response.body).deep_symbolize_keys
-    expect(json[:url]).to eq(["can't be blank"])
-
-    # no new bookmark record is created
-    expect(Bookmark.count).to eq(0)
+    }, headers: { 'X-Username': user.username, 'X-Token': user.authentication_token }
+  # ...
   end
 end
